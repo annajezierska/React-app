ab932f6d4431f11f88bf39da975b35481f4425c1
Anna Jezierska <annamajezierska@gmail.com>
2019-03-31 21:36:12 +0200

    fetching data from weather api

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Title from './components/Title/Title'
 import Form from './components/Form/Form'
 import Weather from './components/Weather/Weather'
-import './App.css';
+import './App.css'
+
+const API_KEY = 'b2916a3a757651b5b75a70112a5bfe10'
 
 class App extends Component {
+
+  getWeather = async (e) => {
+    e.preventDefault()
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Gdansk,Poland&appid=${API_KEY}&units=metric~]`)
+    const data = await api_call.json()
+    console.log(data)
+  }
+
   render() {
     return (
       <div className="App">
        <Title />
-       <Form />
+       <Form getWeather={this.getWeather}/>
        <Weather />
       </div>
     );
--- weather-app/src/components/Form/Form.js
+++ weather-app/src/components/Form/Form.js
@@ -4,7 +4,11 @@ class Form extends Component {
   render() {
     return (
       <div>
-        
+        <form onSubmit={this.props.getWeather}>
+            <input type='text' name='city' placeholder='city'></input>
+            <input type='text' name='country' placeholder='country'></input>
+            <button>Get Weather</button>
+        </form>
       </div>
     )
   }
