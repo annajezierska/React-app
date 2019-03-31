f5126d6a5aec29d182250b322edd0850baeff7dc
Anna Jezierska <annamajezierska@gmail.com>
2019-03-31 21:43:39 +0200

    fetching data for specific cities

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -10,7 +10,9 @@ class App extends Component {
 
   getWeather = async (e) => {
     e.preventDefault()
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Gdansk,Poland&appid=${API_KEY}&units=metric~]`)
+    const city = e.target.elements.city.value
+    const country = e.target.elements.country.value
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric~]`)
     const data = await api_call.json()
     console.log(data)
   }
