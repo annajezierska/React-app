576b3fbd2beae598002e8a9b4100553f964292ae
Anna Jezierska <annamajezierska@gmail.com>
2019-03-31 22:16:53 +0200

    displaying data only when it exists and error added

--- weather-app/src/components/Weather/Weather.js
+++ weather-app/src/components/Weather/Weather.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
- class Weather extends Component {
+class Weather extends Component {
   render() {
     return (
       <div>
-        {this.props.city}
-        {this.props.country}
-        {this.props.temperature}
-        {this.props.humidity}
-        {this.props.description}
+        {this.props.city && this.props.country && (
+          <p>
+            Location: {this.props.city}, {this.props.country}
+          </p>
+        )}
+        {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+        {this.props.description && <p>Conditions: {this.props.description}</p>}
+        {this.props.error && <p>{this.props.error}</p>}
       </div>
-    )
+    );
   }
 }
 export default Weather;
